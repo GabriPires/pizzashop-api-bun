ace230a7a7d8a650ac388aabcc982f79b2feb800
GabriPires <gabpiresdev@gmail.com>
2024-02-15 10:30:55 -0300

    feat: route to create a new restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,38 @@
 import { Elysia } from 'elysia'
+import { z } from 'zod'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello, World!'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
+
+const app = new Elysia().post('/restaurants', async ({ body, set }) => {
+  const createRestaurantBodySchema = z.object({
+    restaurantName: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+    phone: z.string(),
+  })
+
+  const { restaurantName, name, email, phone } =
+    createRestaurantBodySchema.parse(body)
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: users.id,
+    })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    managerId: manager.id,
+  })
+
+  set.status = 204
 })
 
 app.listen(3333, () => {
